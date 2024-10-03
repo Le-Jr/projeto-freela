a184e76dd1935afca237028df594ea92642b62ab
Le-Jr <nascimentoleandrojr@gmail.com>
2024-10-03 19:27:41 -0300

    Correção index.html e adição do arquivo js

---
+++ js/index.js
@@ -0,0 +1,45 @@
+const buttons = document.querySelectorAll(".button"); // Seleciona todos os botões
+const modal = document.querySelector("dialog");
+const modalTitle = document.getElementById("modalTitle");
+const modalContent = document.getElementById("modalContent");
+const closeBtn = document.querySelector(".closeBtn");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // Pega os valores do data-* do botão clicado
+    const title = button.getAttribute("data-title");
+    const content = button.getAttribute("data-content");
+
+    // Atualiza o conteúdo do modal
+    modalTitle.textContent = title;
+    modalContent.textContent = content;
+
+    // Abre o modal
+    modal.showModal();
+  });
+});
+
+closeBtn.addEventListener("click", (e) => {
+  modal.close();
+});
+
+
+
+emailjs.init({
+    publicKey: 'XmsXrHXLJumUYY0IM',
+  });
+emailjs.sendForm('contact_service', 'contact_form', this);
+
+console.log('hw')
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        console.log(emailjs)
+        emailjs.sendForm('service_kxd81p4', 'template_do0cnf8', this)
+            .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
