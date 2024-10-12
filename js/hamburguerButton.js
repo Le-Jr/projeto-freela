c0b77f737ae72f5d614fda00a109f4648bd03a1e
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-10-12 10:24:45 -0300

    alteration
    putting polo cederj in the done works.

--- js/hamburguerButton.js
+++ js/hamburguerButton.js
@@ -3,5 +3,4 @@ navBar = document.querySelector('.navBar');
 
 button.addEventListener('click',()=>{
     navBar.classList.toggle('active-hamburguer')
-    navBar.classList.toggle('w3-hide-small')
 })
