8369320d866c90ebb6d07da3832ee2c4322d2299
EdsonCampanhao <edsonlemos0525@gmail.com>
2024-11-04 10:35:39 -0300

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize('glob_globalnetworks', 'glob_globalnetworks', 'G1lob4ln3twork5', {
-    host: '45.90.223.37',
+const db = new Sequelize('glob_globalnetworks', 'glob_globalnetworks', '', {
+    host: '',
     dialect: 'mysql'
 });
 
